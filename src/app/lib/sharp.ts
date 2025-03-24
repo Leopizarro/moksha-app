95f257b866df75fcc104dadde2de8c116f7b26f3
Leonardo Pizarro <pizarroleonardo.96@gmail.com>
2025-03-24 01:35:55 -0300

    Upload form now supports HEIC images. Also minor changes

--- src/app/lib/sharp.ts
+++ src/app/lib/sharp.ts
@@ -3,13 +3,20 @@ import sharp from "sharp";
 
 export async function resizeImage(
   file: File,
-  width: number
+  dimensions: {
+    width?: number | undefined;
+    height?: number | undefined;
+  }
 ): Promise<Buffer<ArrayBufferLike>> {
+  const { width, height } = dimensions;
+
   const arrayBuffer = await file.arrayBuffer(); // Convert File to ArrayBuffer
   const buffer = Buffer.from(arrayBuffer); // Convert ArrayBuffer to Buffer
 
   // Resize image with sharp
-  const resizedBuffer = await sharp(buffer).resize({ width }).toBuffer();
+  const resizedBuffer = await sharp(buffer)
+    .resize({ width, height })
+    .toBuffer();
 
   // Convert Buffer back to File
   return resizedBuffer;
