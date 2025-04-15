aa5180e0bf9304f41f7415caa158f4f9c731f4bb
Leonardo Pizarro <pizarroleonardo.96@gmail.com>
2025-04-15 11:00:39 -0400

    Now products without image will not be showed

--- src/app/components/products-list/ProductItem.tsx
+++ src/app/components/products-list/ProductItem.tsx
@@ -58,120 +58,122 @@ const ProductItem: React.FC<ProductItemInterface> = (props) => {
 
   return (
     <>
-      <Link
-        className={classes.grow}
-        href={`/product/${product.id}`}
-        style={{ textDecoration: "none" }}
-      >
-        <Card
-          sx={{
-            cursor: "pointer",
-            width: "340px",
-            justifySelf: "center",
-            boxShadow:
-              "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 12px 0 rgba(0, 0, 0, 0.19)",
-          }}
+      {product?.thumbnailImageUrl && (
+        <Link
+          className={classes.grow}
+          href={`/product/${product.id}`}
+          style={{ textDecoration: "none" }}
         >
-          <CardContent sx={{ padding: 0, paddingBottom: 0 }}>
-            <Box
-              className={classes["image-container"]}
-              sx={{
-                position: "relative",
-                minHeight: { xs: "340px" },
-              }}
-            >
-              {session && (
-                <>
-                  <Tooltip title="Eliminar">
-                    <IconButton
-                      aria-label="delete"
-                      size="small"
-                      disabled={openModal}
-                      onClick={(e) => {
-                        e.preventDefault();
-                        e.stopPropagation();
-                        setOpenModal((prevValue) => !prevValue);
-                      }}
-                      sx={{
-                        postion: "absolute",
-                        background: "#d4af37",
-                        margin: "10px",
-                        top: "0",
-                        zIndex: 10,
-                        float: "left",
-                        color: "white",
-                        ":hover": {
-                          background: "#a28834",
-                        },
-                      }}
-                    >
-                      <DeleteIcon fontSize="inherit" />
-                    </IconButton>
-                  </Tooltip>
-                  <Tooltip title="Editar">
-                    <IconButton
-                      disabled={openModal}
-                      aria-label="delete"
-                      size="small"
-                      onClick={(event) => {
-                        event.stopPropagation(); // Prevent the Link click event
-                        event.preventDefault(); // Prevent default behavior
-                        router.push(`/edit/${product.id}`);
-                      }}
-                      sx={{
-                        postion: "absolute",
-                        background: "#d4af37",
-                        top: "0",
-                        margin: "10px",
-                        zIndex: 10,
-                        float: "right",
-                        color: "white",
-                        ":hover": {
-                          background: "#a28834",
-                        },
-                      }}
-                    >
-                      <EditIcon fontSize="inherit" />
-                    </IconButton>
-                  </Tooltip>
-                </>
-              )}
-              <Image
-                alt="thumbnail-image"
-                src={product?.thumbnailImageUrl}
-                quality={100}
-                fill
-                sizes="35vw"
-                style={{
-                  objectFit: "cover",
+          <Card
+            sx={{
+              cursor: "pointer",
+              width: "340px",
+              justifySelf: "center",
+              boxShadow:
+                "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 12px 0 rgba(0, 0, 0, 0.19)",
+            }}
+          >
+            <CardContent sx={{ padding: 0, paddingBottom: 0 }}>
+              <Box
+                className={classes["image-container"]}
+                sx={{
+                  position: "relative",
+                  minHeight: { xs: "340px" },
                 }}
-              />
-            </Box>
-            <Box
-              padding="10px 15px 0px 15px"
-              display="flex"
-              flexDirection="column"
-              rowGap={1}
-            >
-              <Divider
-                textAlign="center"
-                sx={{ fontSize: "12px", color: "gray" }}
               >
-                {product?.productCategory
-                  ?.split("/")
-                  .map((item) => firstLetterUpperCase(item))
-                  .join("/")}
-              </Divider>
-              <Typography variant="h6" component="div">
-                {product?.title.toUpperCase()}
-              </Typography>
-              <Typography variant="body2">
-                {formatToCLP(product?.price)}
-              </Typography>
-            </Box>
-          </CardContent>
-        </Card>
-      </Link>
+                {session && (
+                  <>
+                    <Tooltip title="Eliminar">
+                      <IconButton
+                        aria-label="delete"
+                        size="small"
+                        disabled={openModal}
+                        onClick={(e) => {
+                          e.preventDefault();
+                          e.stopPropagation();
+                          setOpenModal((prevValue) => !prevValue);
+                        }}
+                        sx={{
+                          postion: "absolute",
+                          background: "#d4af37",
+                          margin: "10px",
+                          top: "0",
+                          zIndex: 10,
+                          float: "left",
+                          color: "white",
+                          ":hover": {
+                            background: "#a28834",
+                          },
+                        }}
+                      >
+                        <DeleteIcon fontSize="inherit" />
+                      </IconButton>
+                    </Tooltip>
+                    <Tooltip title="Editar">
+                      <IconButton
+                        disabled={openModal}
+                        aria-label="delete"
+                        size="small"
+                        onClick={(event) => {
+                          event.stopPropagation(); // Prevent the Link click event
+                          event.preventDefault(); // Prevent default behavior
+                          router.push(`/edit/${product.id}`);
+                        }}
+                        sx={{
+                          postion: "absolute",
+                          background: "#d4af37",
+                          top: "0",
+                          margin: "10px",
+                          zIndex: 10,
+                          float: "right",
+                          color: "white",
+                          ":hover": {
+                            background: "#a28834",
+                          },
+                        }}
+                      >
+                        <EditIcon fontSize="inherit" />
+                      </IconButton>
+                    </Tooltip>
+                  </>
+                )}
+                <Image
+                  alt="thumbnail-image"
+                  src={product?.thumbnailImageUrl}
+                  quality={100}
+                  fill
+                  sizes="35vw"
+                  style={{
+                    objectFit: "cover",
+                  }}
+                />
+              </Box>
+              <Box
+                padding="10px 15px 0px 15px"
+                display="flex"
+                flexDirection="column"
+                rowGap={1}
+              >
+                <Divider
+                  textAlign="center"
+                  sx={{ fontSize: "12px", color: "gray" }}
+                >
+                  {product?.productCategory
+                    ?.split("/")
+                    .map((item) => firstLetterUpperCase(item))
+                    .join("/")}
+                </Divider>
+                <Typography variant="h6" component="div">
+                  {product?.title.toUpperCase()}
+                </Typography>
+                <Typography variant="body2">
+                  {formatToCLP(product?.price)}
+                </Typography>
+              </Box>
+            </CardContent>
+          </Card>
+        </Link>
+      )}
       <Modal
         openModal={openModal}
         dialogMessage={`¿Estás seguro de eliminar el producto: ${product.title}?`}
